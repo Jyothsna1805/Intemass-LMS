89e37c2337fde02d7751678d0c5f680bf04c2ca4
Your Name <jyothsnarajavath@gmail.com>
2026-06-09 12:07:48 +0530

    feat: add support for DATABASE_URL in postgres connection string to support Railway 1-click DBs

--- backend/db/database.js
+++ backend/db/database.js
@@ -11,13 +11,17 @@ let dbInstance = null;
 
 const initDb = async () => {
     if (dbType === 'postgres') {
-        dbInstance = new Pool({
-            user: process.env.DB_USER,
-            host: process.env.DB_HOST,
-            database: process.env.DB_NAME,
-            password: process.env.DB_PASSWORD,
-            port: process.env.DB_PORT,
-        });
+        dbInstance = new Pool(
+            process.env.DATABASE_URL 
+                ? { connectionString: process.env.DATABASE_URL }
+                : {
+                    user: process.env.DB_USER,
+                    host: process.env.DB_HOST,
+                    database: process.env.DB_NAME,
+                    password: process.env.DB_PASSWORD,
+                    port: process.env.DB_PORT,
+                }
+        );
 
         // Create tables
         const schemaSql = fs.readFileSync(path.join(__dirname, 'schema.sql')).toString();
